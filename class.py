34c3f03d0667a4c9e1610ccc7779aa01f4135524
Greg17776 <sycraigsabian@gmai.com>
2025-02-22 12:09:22 +0800

    add classCar

---
+++ class.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, brand, age) :
+        self.brand=brand
+        self.age=age
+    def identify(self): 
+        print(' This car is a ' + self.brand + ' age ' + str(self.age))
+    def modifyAge(self, newAge):
+        self.age=newAge
+        print('The new age of the car', self.brand,  'is', self.age)
+        
